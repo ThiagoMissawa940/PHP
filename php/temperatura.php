8d0960e92cfa99b0e1821991598c62f88f98b5cb
ThiagoMissawa940 <thiagomm6@hotmail.com>
2024-10-02 10:58:13 -0300

    Add files via upload

---
+++ php/temperatura.php
@@ -0,0 +1,11 @@
+<?php
+
+$F = readline("digite a temperatura em Fahrenheit:");
+
+$C = (5 * ($F - 32) / 9);
+
+print 'temperatura em farenheit ' .round($F). '';
+
+print ' temperatura em celsius ' .round($C). '';
+
+?> 
---
+++ php/troca.php
@@ -0,0 +1,13 @@
+<?php 
+
+$A = readline("Digite o valor de A:");
+$B = readline("Digite o valor de B:");
+
+$temp = $A;
+$A = $B;
+$B = $temp;
+
+
+echo 'valor de A é igual a ' .$A. ' e valor de B é igual a ' .$B. '';
+
+?>
